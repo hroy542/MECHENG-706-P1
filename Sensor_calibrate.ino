4a1cc6ea5c8d1891d58e4a106b74cadfa1515524
hroy542 <68949721+hroy542@users.noreply.github.com>
2022-03-25 15:25:32 +1300

    KALMAN FILTER WORKS
    Just had the greatest debugging sesh of our lives

--- Sensor_calibrate.ino
+++ Sensor_calibrate.ino
@@ -10,29 +10,26 @@ int Analogue4 = 0; // the read out signal in 0-1023 corresponding to 0-5V
 int Analogue5 = 0; 
 int Analogue6 = 0; 
 int Analogue7 = 0;
-//int distanceLFL = 0; //for the calculated distance using the sensor callibrations
-//int distanceLBL = 0;
-//int distanceLBM = 0;
-//int distanceRBM = 0;
+double distance4 = 0; //for the calculated distance using the sensor callibrations
+double distance5 = 0;
+double distance6 = 0;
+double distance7 = 0;
+double temp = 0;
 
 //----Kalman Filter----
-double Filtered4 = 0;
-double Filtered5 = 0;
-double Filtered6 = 0;
-double Filtered7 = 0;
-double Filtered4_Past = 0;
-double Filtered5_Past = 0;
-double Filtered6_Past = 0;
-double Filtered7_Past = 0;
+double Distance4_Past = 0;
+double Distance5_Past = 0;
+double Distance6_Past = 0;
+double Distance7_Past = 0;
 double last_var4 = 9999;
 double last_var5 = 9999;
 double last_var6 = 9999;
 double last_var7 = 9999;
-double process_noise4 = 0.5;
+double process_noise4 = .05;
 double process_noise5 = 2;
 double process_noise6 = 2;
 double process_noise7 = 2;
-double sensor_noise4 = 9;
+double sensor_noise4 = 25;
 double sensor_noise5 = 7;
 double sensor_noise6 = 7;
 double sensor_noise7 = 7;// Change the value of sensor noise to get different KF performance
@@ -45,9 +42,11 @@ int raw_lipo;
 
 void setup() {
 Serial.begin(115200); // start serial communication
+
 }
 void loop() {
   // put your main code here, to run repeatedly:
+  
   Serial.println("Please enter the mode you want to run: Callibration Mode [1], Battery Test [2] or Enter the Sensor pin for the Sensor you want to tune [4 - 7]?:");
   
   while (Serial.available() ==0){
@@ -80,139 +79,122 @@ void loop() {
     case 2 ... 3:
     while(Serial.available() !=0){
       raw_lipo=analogRead(A0);
-      //Serial.println(raw_lipo);
+      Serial.println(raw_lipo);
       Lipo_level_cal = (raw_lipo - 717);
       Lipo_level_cal = Lipo_level_cal * 100;
       Lipo_level_cal = Lipo_level_cal / 143;
-      Serial.print("Lipo Level: ");
-      Serial.print(Lipo_level_cal);
-      Serial.println("%");
+//      Serial.print("Lipo Level: ");
+//      Serial.print(Lipo_level_cal);
+//      Serial.println("%");
       delay(400);
     }
     break;
     case 4:
       while(1){
         Analogue4 = analogRead(LEFT_FRONT_LIR); // the read out is a signal from 0-1023 corresponding to 0-5v
-        //Testing code goes here
-        Filtered4 = Kalman(Analogue4, Filtered4_Past, process_noise4, sensor_noise4, last_var4);
-        
-        Serial.print(0);
+        temp = Distance(4, Analogue4);
+        Serial.print(temp);
         Serial.print(",");
-        Serial.print(Analogue4);
-        Serial.print(",");
-        Serial.print(Filtered4);
-        Serial.print(",");
-        Serial.println(650);
+//        Serial.println(last_var4);
+        distance4 = Kalman(temp, Distance4_Past, process_noise4, sensor_noise4, last_var4, 4);
+//        Serial.println(last_var4);
+        Serial.println(distance4);
+        Distance4_Past = distance4;
+        delay(25);
       }
     break;
     case 5:
       while(1){
-        Analogue5 = analogRead(LEFT_BACK_LIR);
-  
-        //Testing code goes here
-        Filtered5 = Kalman(Analogue5, Filtered5_Past, process_noise5, sensor_noise5, last_var5);
-        
-        Serial.print(0);
-        Serial.print(",");
-        Serial.print(Analogue5);
-        Serial.print(",");
-        Serial.print(Filtered5);
-        Serial.print(",");
-        Serial.println(650);
+        Analogue5 = analogRead(LEFT_BACK_LIR); // the read out is a signal from 0-1023 corresponding to 0-5v
+        temp = Distance(5, Analogue5);
+        Serial.print(temp);
+        Serial.print(" , ");
+        distance5 = Kalman(temp, Distance5_Past, process_noise5, sensor_noise5, last_var5, 5);
+        Serial.println(distance5);
+        Distance5_Past = distance5;
+        delay(100);
+
       }
     break;
     case 6:
       while(1){
-        Analogue6 = analogRead(LEFT_BACK_MIR);
-  
-        //Testing code goes here
-        Filtered6 = Kalman(Analogue6, Filtered6_Past, process_noise6, sensor_noise6, last_var6);
-        
-        Serial.print(0);
-        Serial.print(",");
-        Serial.print(Analogue6);
-        Serial.print(",");
-        Serial.print(Filtered6);
-        Serial.print(",");
-        Serial.println(650);
+        Analogue6 = analogRead(LEFT_BACK_MIR); // the read out is a signal from 0-1023 corresponding to 0-5v
+        temp = Distance(6, Analogue6);
+        Serial.print(temp);
+        Serial.print(" , ");
+        distance6 = Kalman(temp, Distance6_Past, process_noise6, sensor_noise6, last_var6, 6);
+        Serial.println(distance6);
+        Distance6_Past = distance6;
+        delay(100);
+
       }
     break;
     case 7:
       while(1){
-        Analogue7 = analogRead(RIGHT_BACK_MIR);
-  
-        //Testing code goes here
-        Filtered7 = Kalman(Analogue7, Filtered7_Past, process_noise7, sensor_noise7, last_var7);
-        
-        Serial.print(0);
-        Serial.print(",");
-        Serial.print(Analogue7);
-        Serial.print(",");
-        Serial.print(Filtered7);
-        Serial.print(",");
-        Serial.println(650);
+        Analogue7 = analogRead(RIGHT_BACK_MIR); // the read out is a signal from 0-1023 corresponding to 0-5v
+        temp = Distance(7, Analogue7);
+        Serial.print(temp);
+        Serial.print(" , ");
+        distance7 = Kalman(temp, Distance7_Past, process_noise7, sensor_noise7, last_var7 ,7);
+        Serial.println(distance7);
+        Distance7_Past = distance7;
+        delay(100);
       }
     break;
-    }
-   
-  //int distanceLFL = 17948*pow(signalADC,-1.22); // calculate the distance using the datasheet graph
-  //int distanceLBL = 46161*pow(signalADC,-1.302); // calculate the distance using the calibrated graph
-  //int distanceLBM = 17948*pow(signalADC,-1.22); 
-  //int distanceRBM = 46161*pow(signalADC,-1.302);
+  }
+}
 
- /*Serial.print("distance datasheet graph "); //print out the results on serial monitor
-  Serial.print(distance1);
-  Serial.println("cm");
-  Serial.print(" ");
-  Serial.print("distance calibration graph ");
-  Serial.print(distancec);
-  Serial.println("cm");
-  //Serial.print(0);
-  //Serial.print("");
-  //Serial.print(1000);
-  //Serial.print("");*/
+double Distance(int code, int RawADC) { // find distances using calibration curve equations
+  double dist;
+  
+  switch(code) {
+    case 1 ... 4:
+      dist = 3127.4*pow(RawADC, -.92);//Callibration for Left Front
+//      Serial.println(dist);
+//      dist = 4E-14*pow(RawADC,6) -8E-11*pow(RawADC,5) + 7E-08*pow(RawADC,4) - 3E-05*pow(RawADC,3) + 0.0085*pow(RawADC,2) - 1.262*RawADC + 108.47;
+    break;
+    case 5:
+      dist = 1790.5*pow(RawADC, -.829);//Callibration for Left Back
+//      dist = -3E-11*pow(RawADC,5) + 4E-08*pow(RawADC,4) - 2E-05*pow(RawADC,3) + 0.0072*pow(RawADC,2) - 1.1758*RawADC + 106.25;
+    break;
+    case 6:
+      dist = 1790.5*pow(RawADC, -.829);
+//      dist = -3E-11*pow(RawADC,5) + 4E-08*pow(RawADC,4) - 2E-05*pow(RawADC,3) + 0.0072*pow(RawADC,2) - 1.1758*RawADC + 106.25;
+    break;
+    case 7:
+      dist = 1790.5*pow(RawADC, -.829);
+//      dist = -3E-11*pow(RawADC,5) + 4E-08*pow(RawADC,4) - 2E-05*pow(RawADC,3) + 0.0072*pow(RawADC,2) - 1.1758*RawADC + 106.25;
+    break;
+  }
+  return dist;
 }
 
 //Kalman Filter for IR sensors
-double Kalman(double rawdata, double prev_est, double process_noise, double sensor_noise, double last_var){   // Kalman Filter
+double Kalman(double rawdata, double prev_est, double process_noise, double sensor_noise, double last_var, int sensor){   // Kalman Filter
   double a_priori_est, a_post_est, a_priori_var, a_post_var, kalman_gain;
 
-  a_priori_est = prev_est;  
-  a_priori_var = last_var + process_noise; 
+  a_priori_est = prev_est;
+  a_priori_var = last_var + process_noise;
 
   kalman_gain = a_priori_var/(a_priori_var+sensor_noise);
   a_post_est = a_priori_est + kalman_gain*(rawdata-a_priori_est);
   a_post_var = (1- kalman_gain)*a_priori_var;
-  last_var = a_post_var;
+  
+  switch(sensor) {
+    case 1 ... 4:
+      last_var4 = a_post_var;
+    break;
+    case 5:
+      last_var5 = a_post_var;
+    break;
+    case 6:
+      last_var6 = a_post_var;
+    break;
+    case 7:
+      last_var7 = a_post_var;
+    break;
+  }
+    
   return a_post_est;
 }
-
-/*double IR_filtered(IR code) { // find distances using calibration curve equations
-  double est, dist;
-  int adc;
-  
-  switch(code) {
-    case LEFT_FRONT:
-      adc = analogRead(IR_LONG_1);
-      //dist =
-      break;
-    case LEFT_BACK:
-      adc = analogRead(IR_LONG_2);
-      //dist = 
-      break;
-    case BACK_LEFT:
-      adc = analogRead(IR_MID_1);
-      //dist =
-      break;
-    case BACK_RIGHT:
-      adc = analogRead(IR_MID_2);
-      //dist = 
-      break;
-      
-  est = Kalman(dist, last_est);
-  last_est = est;  
-      
-  // might need delay dunno
-  // random link https://www.norwegiancreations.com/2016/01/tutorial-multiple-values-in-the-arduino-ide-serial-plotter/    
-  return est;
-}*/ 
+ // random link https://www.norwegiancreations.com/2016/01/tutorial-multiple-values-in-the-arduino-ide-serial-plotter/    
