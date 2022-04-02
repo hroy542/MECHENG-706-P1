64f66a96ab3c46c1446e0162e034966670a278f1
FinnM4444 <68797369+FinnM4444@users.noreply.github.com>
2022-04-02 16:12:26 +1300

    Update BetterStraightLine.ino

--- BetterStraightLine.ino
+++ BetterStraightLine.ino
@@ -99,15 +99,18 @@ right_front_motor.attach(right_front);
 
 void loop() { 
   Ultrasound();
+  IR_Sensors();
+  //error[0] = 0;
   error[0] = (reference[0] - Ultradistance);
   //error[1] = reference[1]-IR_wall_dist;
   error[1] = 0;
   error[2] = reference[2]-IR_Angle;
+  //Serial.println(error[2]);
   //error[2] = 0;
   PID_Controller();
   inverse_kinematics();
   set_motor_speed();
-  Serial.println(motor_speed_Value[1]);
+  //Serial.println(motor_speed_Value[1]);
   set_motors();
 }
 
@@ -171,6 +174,7 @@ void Ultrasound(){
   Ultraduration = pulseIn(echoPin, HIGH);
   // Calculating the distance
   Ultradistance = Ultraduration * 0.034 / 2;
+  //Serial.println(Ultradistance);
 //----Ultrasound----
 }
 
@@ -178,10 +182,15 @@ void IR_Sensors(){
   // Get distances from left side of robot to wall
   IR_LONG_1_DIST = IR_dist(LEFT_FRONT);
   IR_LONG_2_DIST = IR_dist(LEFT_BACK);
+  //Serial.println(IR_LONG_2_DIST);
   
   IR_wall_dist = 0.3 * IR_LONG_1_DIST + 0.7 * IR_LONG_2_DIST; // distance of centre of robot to wall
   IR_diff = IR_LONG_1_DIST - IR_LONG_2_DIST; // Difference between long range IRs
+  //Serial.println(IR_diff);
   IR_Angle = atan((IR_diff/IR_Between_Dist));
+  //Serial.println(IR_Angle);
+  //IR_Angle = IR_Angle*(3.1415/180);
+  Serial.println(IR_Angle);
 }
 
 
@@ -193,14 +202,11 @@ double IR_dist(IR code) { // find distances using calibration curve equations
     case LEFT_FRONT:
       adc = analogRead(IR_LONG_1);
       //Serial.println(adc);
-      dist = (21454.2)/(2*pow(adc,1.13));
-      //dist = (21454.2)/(2*(adc^1.13))
-      //Serial.println(dist);
+      dist = (6245.5)/(pow(adc,1.042));
       break;
     case LEFT_BACK:
       adc = analogRead(IR_LONG_2);
-      dist = (21454.2)/(2*pow(adc,1.13));
-      //dist = pow((1790.5 * adc), (-0.829));
+      dist = (2730.4)/(pow(adc,0.901));
       break;
     case BACK_LEFT:
       adc = analogRead(IR_MID_1);
@@ -218,3 +224,4 @@ double IR_dist(IR code) { // find distances using calibration curve equations
       
   return dist;
 }
+
